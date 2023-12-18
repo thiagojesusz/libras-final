e7bf62bcfffcd3d1d5b1208db8e883d7f1087aff
Thiago Silva de Jesus <thiago.jesus@feluma.org.br>
2023-12-18 18:19:56 -0300

    Imagens, Signs, Index atualizados

---
+++ libras-final/src/components/handimage/index.js
@@ -0,0 +1,189 @@
+import A_sign from "./A.svg"
+import B_sign from "./B.svg"
+import C_sign from "./C.svg"
+import D_sign from "./D.svg"
+import E_sign from "./E.svg"
+import F_sign from "./F.svg"
+import G_sign from "./G.svg"
+import H_sign from "./H.svg"
+import I_sign from "./I.svg"
+import J_sign from "./J.svg"
+import K_sign from "./K.svg"
+import L_sign from "./L.svg"
+import M_sign from "./M.svg"
+import N_sign from "./N.svg"
+import O_sign from "./O.svg"
+import P_sign from "./P.svg"
+import Q_sign from "./Q.svg"
+import R_sign from "./R.svg"
+import S_sign from "./S.svg"
+import T_sign from "./T.svg"
+import U_sign from "./U.svg"
+import V_sign from "./V.svg"
+import W_sign from "./W.svg"
+import X_sign from "./X.svg"
+import Y_sign from "./Y.svg"
+import Z_sign from "./Z.svg"
+
+import A_hand from "./Ahand.jpg"
+import B_hand from "./Bhand.jpg"
+import C_hand from "./Chand.jpg"
+import D_hand from "./Dhand.jpg"
+import E_hand from "./Ehand.jpg"
+import F_hand from "./Fhand.jpg"
+import G_hand from "./Ghand.jpg"
+import H_hand from "./Hhand.jpg"
+import I_hand from "./Ihand.jpg"
+import J_hand from "./Jhand.jpg"
+import K_hand from "./Khand.jpg"
+import L_hand from "./Lhand.jpg"
+import M_hand from "./Mhand.jpg"
+import N_hand from "./Nhand.jpg"
+import O_hand from "./Ohand.jpg"
+import P_hand from "./Phand.jpg"
+import Q_hand from "./Qhand.jpg"
+import R_hand from "./Rhand.jpg"
+import S_hand from "./Shand.jpg"
+import T_hand from "./Thand.jpg"
+import U_hand from "./Uhand.jpg"
+import V_hand from "./Vhand.jpg"
+import W_hand from "./Whand.jpg"
+import X_hand from "./Xhand.jpg"
+import Y_hand from "./Yhand.jpg"
+import Z_hand from "./Zhand.jpg"
+
+export const Signimage = {
+  A: A_sign,
+  B: B_sign,
+  C: C_sign,
+  D: D_sign,
+  E: E_sign,
+  F: F_sign,
+  G: G_sign,
+  H: H_sign,
+  I: I_sign,
+  J: J_sign,
+  K: K_sign,
+  L: L_sign,
+  M: M_sign,
+  N: N_sign,
+  O: O_sign,
+  P: P_sign,
+  Q: Q_sign,
+  R: R_sign,
+  S: S_sign,
+  T: T_sign,
+  U: U_sign,
+  V: V_sign,
+  W: W_sign,
+  X: X_sign,
+  Y: Y_sign,
+  Z: Z_sign,
+}
+
+export const Signpass = [
+  {
+    src: A_hand,
+    alt: "A",
+  },
+  {
+    src: B_hand,
+    alt: "B",
+  },
+  {
+    src: C_hand,
+    alt: "C",
+  },
+  {
+    src: D_hand,
+    alt: "D",
+  },
+  {
+    src: E_hand,
+    alt: "E",
+  },
+  {
+    src: F_hand,
+    alt: "F",
+  },
+  {
+    src: G_hand,
+    alt: "G",
+  },
+  {
+    src: H_hand,
+    alt: "H",
+  },
+  {
+    src: I_hand,
+    alt: "I",
+  },
+  {
+    src: J_hand,
+    alt: "J",
+  },
+  {
+    src: K_hand,
+    alt: "K",
+  },
+  {
+    src: L_hand,
+    alt: "L",
+  },
+  {
+    src: M_hand,
+    alt: "M",
+  },
+  {
+    src: N_hand,
+    alt: "N",
+  },
+  {
+    src: O_hand,
+    alt: "O",
+  },
+  {
+    src: P_hand,
+    alt: "P",
+  },
+  {
+    src: Q_hand,
+    alt: "Q",
+  },
+  {
+    src: R_hand,
+    alt: "R",
+  },
+  {
+    src: S_hand,
+    alt: "S",
+  },
+  {
+    src: T_hand,
+    alt: "T",
+  },
+  {
+    src: U_hand,
+    alt: "U",
+  },
+  {
+    src: V_hand,
+    alt: "V",
+  },
+  {
+    src: W_hand,
+    alt: "W",
+  },
+  {
+    src: X_hand,
+    alt: "X",
+  },
+  {
+    src: Y_hand,
+    alt: "Y",
+  },
+  {
+    src: Z_hand,
+    alt: "Z",
+  },
+]
--- libras-final/src/pages/index.js
+++ libras-final/src/pages/index.js
@@ -1,118 +1,254 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useRef, useState, useEffect } from "react"
+import * as handpose from "@tensorflow-models/handpose"
+import Webcam from "react-webcam"
+import { drawHand } from "../components/handposeutil"
+import * as fp from "fingerpose"
+import Handsigns from "../components/handsigns"
+import { useStopwatch } from 'react-timer-hook';
 
-const inter = Inter({ subsets: ['latin'] })
+import { Signimage, Signpass } from "../components/handimage"
+
+import "@tensorflow/tfjs-backend-webgl"
 
 export default function Home() {
+  const webcamRef = useRef(null)
+  const canvasRef = useRef(null)
+
+    const {
+    totalSeconds,
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    reset,
+  } = useStopwatch({ autoStart: true });
+
+  const [camState, setCamState] = useState("on")
+
+  const [sign, setSign] = useState(null)
+
+  let signList = []
+  let currentSign = 0
+
+  let gamestate = "started"
+
+  // let net;
+
+  async function runHandpose() {
+    const net = await handpose.load()
+    _signList()
+
+    // window.requestAnimationFrame(loop);
+
+    setInterval(() => {
+      detect(net)
+    }, 150)
+  }
+
+  function _signList() {
+    signList = generateSigns()
+  }
+
+  function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[a[i], a[j]] = [a[j], a[i]]
+    }
+    return a
+  }
+
+  function generateSigns() {
+    const password = shuffle(Signpass)
+    return password
+  }
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video
+      const videoWidth = webcamRef.current.video.videoWidth
+      const videoHeight = webcamRef.current.video.videoHeight
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth
+      webcamRef.current.video.height = videoHeight
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth
+      canvasRef.current.height = videoHeight
+
+      // Make Detections
+      const hand = await net.estimateHands(video)
+
+      if (hand.length > 0) {
+        //loading the fingerpose model
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.ThumbsUpGesture,
+          Handsigns.aSign,
+          Handsigns.bSign,
+          Handsigns.cSign,
+          Handsigns.dSign,
+          Handsigns.eSign,
+          Handsigns.fSign,
+          Handsigns.gSign,
+          Handsigns.hSign,
+          Handsigns.iSign,
+          Handsigns.jSign,
+          Handsigns.kSign,
+          Handsigns.lSign,
+          Handsigns.mSign,
+          Handsigns.nSign,
+          Handsigns.oSign,
+          Handsigns.pSign,
+          Handsigns.qSign,
+          Handsigns.rSign,
+          Handsigns.sSign,
+          Handsigns.tSign,
+          Handsigns.uSign,
+          Handsigns.vSign,
+          Handsigns.wSign,
+          Handsigns.xSign,
+          Handsigns.ySign,
+          Handsigns.zSign,
+        ])
+
+        const estimatedGestures = await GE.estimate(hand[0].landmarks, 6.5)
+        // document.querySelector('.pose-data').innerHTML =JSON.stringify(estimatedGestures.poseData, null, 2);
+
+        if (gamestate === "started") {
+          document.querySelector("#app-title").innerText =
+            'Faça um ok "👍" com a mão para começar.'
+        }
+
+        if (
+          estimatedGestures.gestures !== undefined &&
+          estimatedGestures.gestures.length > 0
+        ) {
+          const confidence = estimatedGestures.gestures.map(p => p.confidence)
+          const maxConfidence = confidence.indexOf(
+            Math.max.apply(undefined, confidence)
+          )
+
+          //setting up game state, looking for thumb emoji
+          if (
+            estimatedGestures.gestures[maxConfidence]?.name === "thumbs_up" &&
+            gamestate !== "played"
+          ) {
+            _signList()
+            gamestate = "played"
+            document.getElementById("emojimage").classList.add("play")
+            document.querySelector(".tutor-text").innerText =
+              "faça um gesto com a mão baseado na letra mostrada abaixo"
+          } else if (gamestate === "played") {
+            document.querySelector("#app-title").innerText = ""
+
+            //looping the sign list
+            if (currentSign === signList.length) {
+              _signList()
+              currentSign = 0
+              return
+            }
+
+            // console.log(signList[currentSign].src.src)
+
+            //game play state
+
+            if (
+              typeof signList[currentSign].src.src === "string" ||
+              signList[currentSign].src.src instanceof String
+            ) {
+              document
+                .getElementById("emojimage")
+                .setAttribute("src", signList[currentSign].src.src)
+              if (
+                signList[currentSign].alt ===
+                estimatedGestures.gestures[maxConfidence].name
+              ) {
+                currentSign++
+                adicionarSegundosAoStorage(document.querySelector('#totalTimer').textContent)
+                reset();
+              }
+              setSign(estimatedGestures.gestures[maxConfidence].name)
+            }
+          } else if (gamestate === "finished") {
+            return
+          }
+        }
+      }
+      // Draw hand lines
+      const ctx = canvasRef.current.getContext("2d")
+      drawHand(hand, ctx)
+    }
+  }
+
+  function adicionarSegundosAoStorage(segundos) {
+    // Obtém a lista de segundos do sessionStorage (se existir)
+    let listaSegundos = JSON.parse(sessionStorage.getItem('listaSegundos')) || [];
+  
+    // Adiciona os segundos à lista
+    listaSegundos.push(segundos);
+  
+    // Atualiza o sessionStorage com a lista atualizada
+    sessionStorage.setItem('listaSegundos', JSON.stringify(listaSegundos));
+  
+    // Retorna a lista atualizada
+    return listaSegundos;
+  }
+
+  function copiarListaParaClipboard() {
+    // Obtém a lista de segundos do sessionStorage
+    const listaSegundos = JSON.parse(sessionStorage.getItem('listaSegundos')) || [];
+  
+    // Converte a lista para uma string separada por vírgulas
+    const listaComoString = listaSegundos.join(', ');
+  
+    // Cria um elemento de texto temporário para copiar para a área de transferência
+    const elementoTemporario = document.createElement('textarea');
+    elementoTemporario.value = listaComoString;
+  
+    // Adiciona o elemento à página
+    document.body.appendChild(elementoTemporario);
+  
+    // Seleciona e copia o conteúdo do elemento de texto
+    elementoTemporario.select();
+    document.execCommand('copy');
+  
+    // Remove o elemento temporário
+    document.body.removeChild(elementoTemporario);
+  
+    // Exibe uma mensagem ou realiza outras ações conforme necessário
+    alert('Lista copiada para a área de transferência!');
+  }
+  
+
+  //   if (sign) {
+  //     console.log(sign, Signimage[sign])
+  //   }
+
+  useEffect(() => {
+    runHandpose()
+  }, [])
+
+  function turnOffCamera() {
+    if (camState === "on") {
+      setCamState("off")
+    } else {
+      setCamState("on")
+    }
+  }
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="container">
+ 
+    </div>
+  );
+  
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
 }
